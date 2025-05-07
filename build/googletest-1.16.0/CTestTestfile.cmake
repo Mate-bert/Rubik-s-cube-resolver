aa4981e4795559b78ec1c7fcdfd53ff99cd297af
Rayanebouh <rayane.bouhmidi@ensea.fr>
2025-05-07 09:38:59 +0200

    suppression des dossiers inutiles

--- build/googletest-1.16.0/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gnsakawakatamba/Documents/Perso/ecole/Rubik-s-cube-resolver/googletest-1.16.0
-# Build directory: C:/Users/gnsakawakatamba/Documents/Perso/ecole/Rubik-s-cube-resolver/build/googletest-1.16.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- utils/config_loader.py
+++
@@ -1,7 +0,0 @@
-import yaml  # Importation du module YAML pour manipuler des fichiers YAML
-
-def load_config(config_path="data/config/config.yaml"):
-    # Ouvre le fichier de configuration spécifié en mode lecture avec encodage UTF-8
-    with open(config_path, "r", encoding="utf-8") as f:
-        # Charge et retourne le contenu du fichier YAML sous forme de dictionnaire Python
-        return yaml.safe_load(f)
